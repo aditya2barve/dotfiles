4aa217cf3e54e103b54c5f5d5cb46804b15addb8
Aditya Barve <aditya2barve@gmail.com>
2021-04-20 02:08:43 -0700

    set up vimdiff

--- setup.sh
+++ setup.sh
@@ -7,5 +7,9 @@ source ~/repos/dotfiles/bashrc
 
 ln -s ~/repos/dotfiles/vimrc ~/.vimrc
 
+# configure vimdiff as git difftool
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+
 echo "setup complete!"
 
