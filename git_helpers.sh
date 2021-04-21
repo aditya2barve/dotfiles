eadbde85ad66ff0027721d27f3e82cc1b4cf0e71
Aditya Barve <aditya2barve@gmail.com>
2021-04-21 01:24:15 -0700

    gll show files changed

--- git_helpers.sh
+++ git_helpers.sh
@@ -5,7 +5,7 @@ alias gcu="git reset HEAD~1"
 alias gd="git diff"
 alias gdd="git difftool"
 alias gl="git log"
-alias gll="git log -1 HEAD"
+alias gll="git log -1 --stat"
 alias glo="git log -20 --oneline"
 alias gs="git status"
 
