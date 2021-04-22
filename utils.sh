d8a798ba3bc23b9b432b5a4a3e70f044d76fa510
Aditya Barve <aditya2barve@gmail.com>
2021-04-22 14:02:12 -0700

    fix open_in_browser for WSL2

--- utils.sh
+++ utils.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 function open_in_browser() {
+    unameOut=$(uname -a)
+    if [[ $unameOut == *WSL2* ]]; then
+        # echo detected WSL2
+        sensible-browser "$1"
+        # if sensible-browser doesn't work, try this:
+        # powershell.exe /c start $1
+        return
+    fi
     open -a firefox -g $1
 }
 
