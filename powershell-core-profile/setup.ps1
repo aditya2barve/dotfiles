bcb599e8d46961a4f6b9665c468f8c242e6a125f
Aditya Barve <aditya2barve@gmail.com>
2021-08-13 18:42:11 -0700

    exit early

--- powershell-core-profile/setup.ps1
+++ powershell-core-profile/setup.ps1
@@ -1,8 +1,8 @@
 $setupAlreadyExists = (Get-Content -Path $PROFILE.CurrentUserAllHosts) -match "commands.ps1"
 if ($setupAlreadyExists) {
     Write-Host "setup already exists."
+    return
 }
-else {
-    Get-Content -Path ./appendToProfile.ps1 | Add-Content -Path $PROFILE.CurrentUserAllHosts
-    Write-Host "appended to profile."
-}
+
+Get-Content -Path ./appendToProfile.ps1 | Add-Content -Path $PROFILE.CurrentUserAllHosts
+Write-Host "appended to profile."
